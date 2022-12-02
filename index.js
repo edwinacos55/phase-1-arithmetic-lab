b4e0e2fd075d73999f98b437eee6f4ff37089cac
edwinacos55 <edwinacos55@gmail.com>
2022-12-02 17:20:16 -0500

    commit tree efce95c9217b91ad593a6dca996073c0cb5c8501

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+// Write your code here'
+
+    const num1 = 31;
+    const num2= 2;
+    let multiply = num2 * num1;
+
+    let random= Math.floor(Math.random() * 10) + 1;
+    const num3= 4;
+    const num4= 16;
+    let mod= num3 % num4;
+
+let max = max[10, 8, 7, 20]; 
